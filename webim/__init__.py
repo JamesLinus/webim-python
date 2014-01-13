8e7152d337021bfbadd6c18ccf7c41ef62dd5e57
Ery Lee <ery.lee@gmail.com>
2014-01-13 12:59:42 +0800

    1.0.1

--- webim/__init__.py
+++ webim/__init__.py
@@ -1,6 +1,6 @@
 
 __author__    = "Ery Lee <ery.lee@gmail.com>"
-__version__ = "1.0"
+__version__ = "1.0.1"
 __copyright__ = "Copyright (C) 2014 Ery Lee"
 __license__   = "Python Software Foundation License"
 
